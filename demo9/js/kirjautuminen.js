8b9e9da9bb9c13ad0a59638ad5bd9a0a9977ff45
JamiLKeuda <126817251+JamiLKeuda@users.noreply.github.com>
2023-06-22 09:47:24 +0300

    demo9 script harkka

---
+++ demo9/js/kirjautuminen.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", onkoKirjautunut);
+
+function onkoKirjautunut(){
+    if (localStorage.getItem("kirjautunut") === "kyllä") {
+        document.getElementById("tervetulo_teksti").textContent += localStorage.getItem("nimi");
+        document.getElementById("kirjautumis_lomake").style.display = "none";
+    } else {
+        document.getElementById("kirjaudu_ulos").style.display = "none";
+        document.getElementById("tervetulo_teksti").style.display = "none";
+    }
+}
+
+function kirjaudu() {
+    localStorage.setItem("nimi", document.getElementById("nimi").value);
+    localStorage.setItem("kirjautunut", "kyllä");
+}
+
+function kirjauduUlos() {
+    localStorage.removeItem("nimi");
+    localStorage.removeItem("kirjautunut")
+    document.getElementById("tervetuloa_teksti").textContent = "Tervetuloa";
+    document.getElementById("nimi").value = "";
+}
